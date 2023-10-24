f8bb6d30a26951c3407ce0a1a8779489e0dd3ef4
wendeldevilla <devillawendel2731@gmail.com>
2023-10-25 02:05:36 +0800

    update delete address added

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,15 +1,117 @@
 "use client";
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context";
 import { addNewAddressFormControls } from "../utils";
 import InputComponent from "../components/FormELements/InputElement";
+import {
+  addNewAddress,
+  deleteAddress,
+  fetchAllAddresses,
+  updateAddress,
+} from "@/services/address";
+import { toast } from "react-toastify";
+import Notification from "../components/Notification";
+import ComponentLevelLoader from "../components/Loader/component/componentlevel";
+import { PulseLoader } from "react-spinners";
 
 export default function Account() {
-  const { user, addresses, setAddresses, addressFormData, setAddressFormData } =
-    useContext(GlobalContext);
+  const {
+    user,
+    addresses,
+    setAddresses,
+    addressFormData,
+    setAddressFormData,
+    componentLevelLoader,
+    setComponentLevelLoader,
+    pageLevelLoader,
+    setPageLevelLoader,
+  } = useContext(GlobalContext);
 
   const [showAddressForm, setShowAddressForm] = useState(false);
+  const [currentEditedAddressId, setCurrentEditedAddressId] = useState(null);
+
+  async function extractAllAddresses() {
+    setPageLevelLoader(true);
+    const res = await fetchAllAddresses(user?._id);
+
+    if (res.success) {
+      setPageLevelLoader(false);
+      setAddresses(res.data);
+    }
+  }
+
+  async function handleAddOrUpdateAddress() {
+    setComponentLevelLoader({ loading: true, id: "" });
+    const res =
+      currentEditedAddressId !== null
+        ? await updateAddress({
+            ...addressFormData,
+            _id: currentEditedAddressId,
+          })
+        : await addNewAddress({ ...addressFormData, userID: user?._id });
+
+    console.log(res);
+
+    if (res.success) {
+      setComponentLevelLoader({ loading: false, id: "" });
+      toast.success(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setAddressFormData({
+        fullName: "",
+        province: "",
+        zipCode: "",
+        address: "",
+      });
+      extractAllAddresses();
+      setCurrentEditedAddressId(null);
+    } else {
+      setComponentLevelLoader({ loading: false, id: "" });
+      toast.error(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setAddressFormData({
+        fullName: "",
+        province: "",
+        zipCode: "",
+        address: "",
+      });
+    }
+  }
+
+  function handleUpdateAddress(getCurrentAddress) {
+    setShowAddressForm(true);
+    setAddressFormData({
+      fullName: getCurrentAddress.fullName,
+      province: getCurrentAddress.province,
+      zipCode: getCurrentAddress.zipCode,
+      address: getCurrentAddress.address,
+    });
+    setCurrentEditedAddressId(getCurrentAddress._id);
+  }
+
+  async function handleDelete(getCurrentAddressID) {
+    setComponentLevelLoader({ loading: true, id: getCurrentAddressID });
+    const res = await deleteAddress(getCurrentAddressID);
+
+    if (res.success) {
+      setComponentLevelLoader({ loading: false, id: "" });
+      toast.success(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      extractAllAddresses();
+    } else {
+      setComponentLevelLoader({ loading: false, id: "" });
+      toast.error(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (user !== null) extractAllAddresses();
+  }, [user]);
 
   return (
     <section>
@@ -29,26 +131,54 @@ export default function Account() {
             </button>
             <div className="mt-6">
               <h1 className="font-bold text-lg">Your Addresses :</h1>
-              <div className="mt-4">
-                {addresses && addresses.length ? (
-                  addresses.map((item) => (
-                    <div className="border p-6" key={item._id}>
-                      <p>Name : {item.fullName}</p>
-                      <p>Address : {item.address}</p>
-                      <p>Province : {item.province}</p>
-                      <p>Zip Code : {item.zipCode}</p>
-                      <button className="mt-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide">
-                        Update
-                      </button>
-                      <button className="mt-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide">
-                        Delete
-                      </button>
-                    </div>
-                  ))
-                ) : (
-                  <p>No Addresses found. Please add a new address !</p>
-                )}
-              </div>
+              {pageLevelLoader ? (
+                <PulseLoader
+                  color={"#000000"}
+                  loading={pageLevelLoader}
+                  size={15}
+                  data-testid="loader"
+                />
+              ) : (
+                <div className="mt-4 flex flex-col gap-4">
+                  {addresses && addresses.length ? (
+                    addresses.map((item) => (
+                      <div className="border p-6" key={item._id}>
+                        <p>Name : {item.fullName}</p>
+                        <p>Address : {item.address}</p>
+                        <p>Province : {item.province}</p>
+                        <p>Zip Code : {item.zipCode}</p>
+                        <button
+                          onClick={() => handleUpdateAddress(item)}
+                          className="mt-5 mr-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                        >
+                          Update
+                        </button>
+                        <button
+                          onClick={() => handleDelete(item._id)}
+                          className="mt-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                        >
+                          {componentLevelLoader &&
+                          componentLevelLoader.loading &&
+                          componentLevelLoader.id === item._id ? (
+                            <ComponentLevelLoader
+                              text={"Deleting"}
+                              color={"#ffffff"}
+                              loading={
+                                componentLevelLoader &&
+                                componentLevelLoader.loading
+                              }
+                            />
+                          ) : (
+                            "Delete"
+                          )}
+                        </button>
+                      </div>
+                    ))
+                  ) : (
+                    <p>No Addresses found. Please add a new address !</p>
+                  )}
+                </div>
+              )}
             </div>
             <div className="mt-4">
               <button
@@ -76,14 +206,28 @@ export default function Account() {
                     />
                   ))}
                 </div>
-                <button className="mt-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide">
-                  Save
+                <button
+                  onClick={handleAddOrUpdateAddress}
+                  className="mt-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                >
+                  {componentLevelLoader && componentLevelLoader.loading ? (
+                    <ComponentLevelLoader
+                      text={"Saving"}
+                      color={"#ffffff"}
+                      loading={
+                        componentLevelLoader && componentLevelLoader.loading
+                      }
+                    />
+                  ) : (
+                    "Save"
+                  )}
                 </button>
               </div>
             ) : null}
           </div>
         </div>
       </div>
+      <Notification />
     </section>
   );
 }
