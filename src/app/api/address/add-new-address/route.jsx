ceaada0a5353f29a3cfabad559b4070ec6601d83
wendeldevilla <devillawendel2731@gmail.com>
2023-10-24 23:51:21 +0800

    address api created

---
+++ src/app/api/address/add-new-address/route.jsx
@@ -0,0 +1,69 @@
+import connectToDB from "@/database";
+import AuthUser from "@/middleware/AuthUser";
+import Address from "@/models/address";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+
+const AddNewAddress = Joi.object({
+  fullName: Joi.string().required(),
+  address: Joi.string().required(),
+  city: Joi.string().required(),
+  zipCode: Joi.string().required(),
+  userID: Joi.string().required(),
+});
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+  try {
+    await connectToDB();
+
+    const isAuthUser = await AuthUser(req);
+
+    if (isAuthUser) {
+      const data = await req.json();
+
+      const { fullName, address, city, zipCode, userID } = data;
+
+      const { error } = AddNewAddress.validate({
+        fullName,
+        address,
+        city,
+        zipCode,
+        userID,
+      });
+
+      if (error) {
+        return NextResponse.json({
+          success: false,
+          message: error.details[0].message,
+        });
+      }
+
+      const newlyAddedAddress = await Address.create(data);
+
+      if (newlyAddedAddress) {
+        return NextResponse.json({
+          success: true,
+          message: "Address added successfully",
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "Failed to add address. Please try again later !",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong. Please try again later !",
+    });
+  }
+}
---
+++ src/app/api/address/update-address/route.jsx
@@ -0,0 +1,50 @@
+import connectToDB from "@/database";
+import AuthUser from "@/middleware/AuthUser";
+import Address from "@/models/address";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function PUT(req) {
+  try {
+    await connectToDB();
+
+    const isAuthUser = await AuthUser(req);
+
+    if (isAuthUser) {
+      const data = await req.json();
+      const { _id, fullname, city, address, zipCode } = data;
+
+      const updateAddress = await Address.findOneAndUpdate(
+        {
+          _id: _id,
+        },
+        { _id, fullname, city, address, zipCode },
+        { new: true }
+      );
+
+      if (updateAddress) {
+        return NextResponse.json({
+          success: true,
+          message: "Address updated successfully !",
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "Failed to udpate address. Please try again !",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong. Please try again later !",
+    });
+  }
+}
